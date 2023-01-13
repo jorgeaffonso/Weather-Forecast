934037a05da91d53f39dc4e55ccb92b185a740e4
Jorge <jorgemail.affonso@gmail.com>
2023-01-13 18:17:54 -0300

    Day 33: Plot line graphic for temperatures in Celsius

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 
-st.title("Weather Forecast for the Next Days")
+st.title("Weather Forecast for the Next day")
+
 place = st.text_input("Place: ")
 days = st.slider('Forecast Days', min_value=1, max_value=5,
                 help="Select the number of forecasted days")
@@ -10,14 +12,7 @@ days = st.slider('Forecast Days', min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10","2022-27-10"]
-    temperatures = [10,11,15]
-    temperatures = [i*days for i in temperatures]
-    return dates, temperatures
-
-d, t = get_data(days)
+d, t = get_data(place, days, option)
 
 # Lib plotly or bokeh
 figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
