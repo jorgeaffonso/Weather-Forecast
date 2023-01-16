2acc763ace661026016d7ba9142b6cb5f853c2f9
Jorge <jorgemail.affonso@gmail.com>
2023-01-16 16:41:30 -0300

    Day 33: Plot sky conditions

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 import requests
 
 API_KEY = "c875c2c90c77edb31d4b66c70dbcf470"
-def get_data(place,forecast_days=None, kind=None):
+def get_data(place,forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
@@ -9,17 +9,7 @@ def get_data(place,forecast_days=None, kind=None):
     filtered_data = data["list"]
     nr_values = forecast_days * 8      # 1 day = 24/3 = 8  3hs entries
     filtered_data = filtered_data[:nr_values]
-#    print(filtered_data[0]['dt_txt'])   # print date and hour for the first element
-    dates = [dic["dt_txt"] for dic in filtered_data]
-#   print(dates)
-
-    if kind == "Temperature":
-#       filtered_data = [float("{:.2f}".format(dic["main"]['temp'])) for dic in filtered_data]  - transforma em float com 2 casas dec
-        filtered_data = [dic["main"]['temp']-273.15 for dic in filtered_data]  # from Kelvin to celsius
-    if kind == "Sky":
-        filtered_data = [dic["weather"][0]['main'] for dic in filtered_data]
-
-    return dates, filtered_data
+    return filtered_data
 
 if __name__ == "__main__":
      print(get_data(place="Rio de Janeiro", forecast_days=3, kind="Temperature"))
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
-
+# Add title, text imput, slider, selecbox and subheader
 st.title("Weather Forecast for the Next day")
 
 place = st.text_input("Place: ")
@@ -12,8 +12,24 @@ days = st.slider('Forecast Days', min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data(place, days, option)
 
-# Lib plotly or bokeh
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+
+if place:
+    # Get the temperature / sky data
+    filtered_data = get_data(place, days)
+
+    if option == "Temperature":
+        #       filtered_data = [float("{:.2f}".format(dic["main"]['temp'])) for dic in filtered_data]  - transforma em float com 2 casas dec
+        temperatures = [dic["main"]['temp'] - 273.15 for dic in filtered_data]  # from Kelvin to celsius
+        dates = [dic["dt_txt"] for dic in filtered_data]
+
+        # Create a temperature plot -  Lib plotly or bokeh
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png","Snow":"images/snow.png"}
+        sky_conditions = [dic["weather"][0]['main'] for dic in filtered_data]
+        print(sky_conditions)
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths,width=115)
